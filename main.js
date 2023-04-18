bf8c29dd14e1fd7af29681ca8d63493e786fa5bc
oranze413 <131138692+oranze413@users.noreply.github.com>
2023-04-18 22:06:30 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
+// window.onload = function(){
+//     // alert("hi");
+// };
+
+let imageURL_Array = [
+    "https://photo.518.com.tw/selfmedia/articles/1822/166184376108829.jpeg",
+    "https://lordcat.tw/wp-content/uploads/2021/09/1631538408-378fce845ce05de4c29be3e870b50e13.jpg",
+    "https://pic03.eapple.com.tw/siangnong/xn_i_img02.png",
+    "https://tokyo-kitchen.icook.network/uploads/recipe/cover/372073/60ad2eda9638fa38.jpg"
+];
+
+
 $(function(){
     $("input").on("click",function(){
-        //alert("Hi");
-        var numberOfListItem=$("li").length;
-        var randomChildNumber=Math.floor(Math.random()*numberOfListItem);
-        var selectedItem = $("li").eq(randomChildNumber).text();
-        $("h1").text(selectedItem);
-        
-        // Check selected item and show corresponding image
-        switch(selectedItem) {
-            case "拉麵":
-                $("#image-container").html("<img src='拉麵.jpg'>");
-                break;
-            case "滷肉飯":
-                $("#image-container").html("<img src='滷肉飯.jpg'>");
-                break;
-            case "水餃":
-                $("#image-container").html("<img src='水餃.jpg'>");
-                break;
-            default:
-                $("#image-container").html("");
-        }
+        // alert("Hi");
+        // $("h1").text("Hello");
+        var numberOfListItem = $("li").length;
+        // 3
+        // 0 ~ 1 => 0 ~ 3
+        var randomChildNumber = Math.floor(Math.random()*numberOfListItem);
+        console.log(randomChildNumber);
+        $("h1").text($("li").eq(randomChildNumber).text());
+        // change image -> change image element's src
+        $("img").attr("src",imageURL_Array[randomChildNumber]);
     });
 });
