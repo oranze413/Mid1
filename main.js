0e29584d632d61b4fcecd6bd6d9bf5f14e09a1d8
oranze413 <131138692+oranze413@users.noreply.github.com>
2023-04-18 21:59:21 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-window.onload=function(){
-    //document.write("HelloJavaScript");
-};
-$(function(){
-    $("input").on("click",function(){
-        alert("Hi");
-    });
-});
 $(function(){
     $("input").on("click",function(){
         //alert("Hi");
         var numberOfListItem=$("li").length;
         var randomChildNumber=Math.floor(Math.random()*numberOfListItem);
-        $("h1").text($("li").eq(randomChildNumber).text());
+        var selectedItem = $("li").eq(randomChildNumber).text();
+        $("h1").text(selectedItem);
+        
+        // Check selected item and show corresponding image
+        switch(selectedItem) {
+            case "拉麵":
+                $("#image-container").html("<img src='拉麵.jpg'>");
+                break;
+            case "滷肉飯":
+                $("#image-container").html("<img src='滷肉飯.jpg'>");
+                break;
+            case "水餃":
+                $("#image-container").html("<img src='水餃.jpg'>");
+                break;
+            default:
+                $("#image-container").html("");
+        }
     });
 });
